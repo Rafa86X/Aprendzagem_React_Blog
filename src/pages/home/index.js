9e911b2f61c69cb53c2581c5cb0dab5564245459
Rafael Junges <rafadevweb@gmail.com>
2023-11-13 13:59:27 -0300

    Codando...

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,19 @@
+import Banner from "../../components/banner";
+import style from './style.module.css'
+
+import posts from '../../posts/posts.json'
+import Post from "../../components/post";
+
 export default function Home(){
     return(
-        <div>Home Home</div>
+        <main>
+            <Banner/>
+            <ul className={style.posts}>
+                {posts.map((p)=>(
+                    <li key={p.id}><Post post={p}/></li>
+                ))}
+            </ul>
+        </main>
+
     )
 }
